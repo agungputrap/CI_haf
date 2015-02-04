e183d10f1a712f7fb3e6bd3cbf2b63daff21cf95
Agung Putra Pasaribu <real.stereo2@gmail.com>
2015-02-04 12:39:18 +0700

    fix username setelah login

--- application/views/pembayaran_siswa_view.php
+++ application/views/pembayaran_siswa_view.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4 well">
-			<h1> <?php echo ($data[0]['Nama']) ?> </h1>
+			<h1 align="center"> <?php echo ($data[0]['Nama']) ?> </h1>
 			<img src="<?php echo base_url("assets/images/bonie.jpg"); ?>" class="img-responsive img-haf" alt="Responsive image">
 		</div>
 		<div class="col-md-6 col-md-offset-1">
